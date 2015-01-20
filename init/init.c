befc9f5d0a41a8cf92daf509b7f072e2f997722c
wlq <wlq@rock-chips.com>
2015-01-20 10:53:29 +0800

    add ptest function

--- init/init.c
+++ init/init.c
@@ -1173,8 +1173,6 @@ int main(int argc, char **argv)
 	init_parse_config_file("/init.ptest.rc");
     else
 	init_parse_config_file("/init.rc");
-	//init_parse_config_file("/init.ptest.rc");
-    //init_parse_config_file("/init.rc");
 
     action_for_each_trigger("early-init", action_add_queue_tail);
 
