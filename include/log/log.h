6acf8ea4cbb320e5bc7784b509e7260118b0515c
Zorro Liu <lyx@rock-chips.com>
2015-02-05 17:17:55 +0800

    log: add logd kernel

--- include/log/log.h
+++ include/log/log.h
@@ -551,7 +551,7 @@ typedef enum log_id {
     LOG_ID_EVENTS = 2,
     LOG_ID_SYSTEM = 3,
     LOG_ID_CRASH = 4,
-
+    LOG_ID_KERNEL = 5,
     LOG_ID_MAX
 } log_id_t;
 #define sizeof_log_id_t sizeof(typeof_log_id_t)
